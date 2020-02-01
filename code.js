0431b7368d2329620e63fbd326994867080da0fa
Oscar Nunez <noscar07@gmail.com>
2020-02-01 00:06:12 -0500

    added moment library

---
+++ code.js
@@ -0,0 +1,13 @@
+/* 
+Building our app should involve in 
+1, Making 8 rows from 9:00 AM - 5:00 PM
+2, display the date on the top.
+3, In each column they will add any text.
+4, Once the text has been complete the client will press the save button. (This action will save the text into local storage.)
+5, Depending on the time of the day the rows will change different colors 
+*/
+
+<script src="moment.js"></script>
+<script>
+    moment().format();
+</script>
