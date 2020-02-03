e29c53127bae4e6d6bb4406d7b5ee3bf4651eba0
Oscar Nunez <noscar07@gmail.com>
2020-02-03 17:49:16 -0500

    added local storage and fixed time to update

--- code.js
+++ code.js
@@ -1,3 +1,41 @@
+function SaveInformation(){
+    console.log($("#nine").val());
+    localStorage.setItem("9",$("#nine").val());
+    localStorage.setItem("10",$("#ten").val());
+    localStorage.setItem("11",$("#eleven").val());
+    localStorage.setItem("12",$("#twelve").val());
+    localStorage.setItem("1",$("#one").val());
+    localStorage.setItem("2",$("#two").val());
+    localStorage.setItem("3",$("#three").val());
+    localStorage.setItem("4",$("#four").val());
+    localStorage.setItem("5",$("#five").val());
+}
+function getInformation(){
+    $("#nine").val(localStorage.getItem("9"));
+    $("#ten").val(localStorage.getItem("10"));
+    $("#eleven").val(localStorage.getItem("11"));
+    $("#twelve").val(localStorage.getItem("12"));
+    $("#one").val(localStorage.getItem("1"));
+    $("#two").val(localStorage.getItem("2"));
+    $("#three").val(localStorage.getItem("3"));
+    $("#four").val(localStorage.getItem("4"));
+    $("#five").val(localStorage.getItem("5"));
+}
+
+var dateTime = null,
+    date = null;
+
+var update = function() {
+    date = moment(new Date())
+    dateTime.html(date.format('dddd, MMMM Do YYYY, h:mm:ss a'));
+};
+$(document).ready(function(){
+    dateTime= $("#currentDay")
+    update();
+    setInterval(update, 1000);
+});
+
+
 /* 
 Building our app should involve in 
 1, Making 8 rows from 9:00 AM - 5:00 PM
@@ -9,8 +47,7 @@ Thing that have been completed
 added moment JS library.
 rows and collums have been added
 text input has been added 
-time on the top has been added 
+time on the top has been added
+local storage has been added
+
 */
-
-
-
